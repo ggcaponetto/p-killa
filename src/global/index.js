1b575c28c0f191e26fe9d2e9847997dea5a23fb3
Giuseppe Caponetto <giuseppe.caponetto@geoimpact.ch>
2019-01-31 16:44:08 +0100

    wip windows listing of pids

--- src/global/index.js
+++ src/global/index.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 // @flow
+const {spawn} = require('child_process');
 
 const getArgvOptions = () => {
   let options = [
@@ -22,7 +23,7 @@ const parseArgv = (argv, argvOptions) => {
 
       let runsWithOption = false;
       argvOption.identifiers.forEach(identifier => {
-        if(identifier === arg){
+        if (identifier === arg) {
           runsWithOption = true;
         }
       });
@@ -35,12 +36,50 @@ const parseArgv = (argv, argvOptions) => {
   return options;
 };
 
+const listProcessedWin = (parsedOptions) => {
+  return new Promise((res, rej) => {
+    var listPidBatPath = require.resolve(`${__dirname}/../../scripts/win/list-pid.bat`);
+    let portOption = parsedOptions.filter((option) => {
+      return option.name === "port";
+    })[0];
+    console.log(`executing ${listPidBatPath} with arg:\n ${JSON.stringify(portOption)}`);
+    const command = spawn(listPidBatPath, [`${portOption.value}`]);
+    command.stdout.on('data', (data) => {
+      console.log(`process stdout:\n ${data}`);
+    });
+    command.stderr.on('data', (data) => {
+      console.log(`process stderr:\n ${data}`);
+    });
+    command.on('close', (code) => {
+      if (code === 0) {
+        res(`process exited with code ${code}`);
+      } else {
+        rej(`process exited with code ${code}`);
+      }
+    });
+  });
+};
+
+const listProcesses = (parsedOptions) => {
+  let platform = process.platform;
+  if (platform === "win32") {
+    console.error(`platform ${platform} is supported`);
+    return listProcessedWin(parsedOptions);
+  } else {
+    console.error(`platform ${platform} not supported yet`);
+  }
+};
+
 const run = () => {
   let argv = process.argv;
   console.log(`running with arguments: \n ${argv.join("\n")}`);
   let argvOptions = getArgvOptions();
   let parsedOptions = parseArgv(argv, argvOptions);
   console.log(`parsed options: \n ${JSON.stringify(parsedOptions, null, 4)}`);
+  return listProcesses(parsedOptions).catch(e => {
+    console.error(e);
+    process.exit(1);
+  });
 };
 
 run();
